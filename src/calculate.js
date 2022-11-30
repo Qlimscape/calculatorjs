b7d20b7a48edb1e4d5e29ff21577788ea8a77649
qlimscape <Jakob.Klimczok@quinscape.de>
2022-11-30 14:05:23 +0100

    add parsing

--- src/calculate.js
+++ src/calculate.js
@@ -1,6 +1,18 @@
-
-
-
+//Main function. Only delegates and returns 
 export default function calculate(operation) {
-	console.log(operation)
+
+	const operationList = parse(operation);
+	console.log(JSON.stringify(operationList));
+}
+
+// parse the operation into an array where operands and operators result in respective values
+function parse(operation) {
+
+	const operationList = [];
+	// split by operators but keep them also => ()
+	operationList.push(operation.split(/([()+x\-/s])/).filter(function (e) {
+		return e !== "";
+	}));
+
+	return operationList;
 }
